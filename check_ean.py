4e51d7b83b5ef01a8f6853444bcb2023ffbfe24a
Alexey Obukhov <34322911+vertok@users.noreply.github.com>
2024-09-19 14:50:48 +0200

    logger adjusted

--- check_ean.py
+++ check_ean.py
@@ -1,6 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QLabel
 
+import logging
+from log import setup_logger, parse_args
+
 def calculate_ean(ean: str) -> bool:
     """
     Calculate the checksum of an EAN-13 number and verify its validity.
@@ -24,10 +27,18 @@ def calculate_ean(ean: str) -> bool:
     check_digit = (10 - (checksum % 10)) % 10
     return check_digit == int(ean[-1])
 
+def config_logger():
+    verbose_level_str = parse_args()
+    verbose_level = getattr(logging, verbose_level_str.upper(), logging.INFO)
+    setup_logger(verbose_level)
+    logger = logging.getLogger('ui')
+    logger.debug("verbose level was set to %s", verbose_level)
+    return logger
+
 class EANChecker(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.logger = logging.getLogger("checker")
         self.initUI()
 
     def initUI(self):
@@ -53,12 +64,20 @@ class EANChecker(QWidget):
         """Check the validity of the entered EAN-13 number and display the result."""
         ean = self.ean_input.text()
         if calculate_ean(ean):
+            self.logger.info("%s is a correct EAN-13 number.", ean)
             self.result_label.setText(f"{ean} is a correct EAN-13 number.")
         else:
+            self.logger.error("%s is not a correct EAN-13 number.", ean)
             self.result_label.setText(f"{ean} is not a correct EAN-13 number.")
+            return
+
 
 if __name__ == '__main__':
+    # config logger
+    logger = config_logger()
+    logger.info("building ui...")
     app = QApplication(sys.argv)
     ex = EANChecker()
+    logger.info("app was build and will be shown straight ahead on the screen.")
     ex.show()
     sys.exit(app.exec())
